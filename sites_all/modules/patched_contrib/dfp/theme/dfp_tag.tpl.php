274f6fc0e57d53c17e6bf1d478b73c9e7d1ee581
Nathan Lisgo <nathan@i-kos.com>
2013-06-03 15:59:11 +0100

    DFP module is modified

---
+++ sites_all/modules/patched_contrib/dfp/theme/dfp_tag.tpl.php
@@ -0,0 +1,14 @@
+<?php
+  /**
+   * @file
+   * Default template for dfp tags.
+   */
+?>
+
+<div <?php print drupal_attributes($placeholder_attributes) ?>>
+  <script type="text/javascript">
+    googletag.cmd.push(function() {
+      googletag.display("<?php print $tag->placeholder_id ?>");
+    });
+  </script>
+</div>
