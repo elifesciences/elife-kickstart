6046168586bdb2dbcef4df267993d1fcc88dda6b
Nathan Lisgo <nathan@i-kos.com>
2013-06-04 12:35:14 +0100

    Improvements to code. Added opportunity to not create template install profile. Improved cleanup after preparation.

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,7 +4,7 @@ set -e
 #
 # Build the distribution using the same process used on Drupal.org
 #
-# Usage: scripts/build.sh [-y] <destination> from the profile main directory.
+# Usage: scripts/build.sh [-y] <destination> <profile_name> <profile_code> from the profile main directory.
 #
 
 confirm () {
@@ -39,18 +39,30 @@ realpath () {
 }
 
 usage() {
-  echo "Usage: build.sh [-y] <DESTINATION_PATH>" >&2
+  echo "Usage: build.sh [-y] <DESTINATION_PATH> <PROFILE_NAME> <PROFILE_CODE>" >&2
   echo "Use -y to skip deletion confirmation" >&2
   exit 1
 }
 
+profiles_folder=0
+if [ -d profiles ]; then
+  profiles_folder=1
+fi
+
+profile_name_def="LN"
+profile_code_def="ln"
+
 DESTINATION=$1
+PROFILE_NAME=${2:-${profile_name_def}}
+PROFILE_CODE=${3:-${profile_code_def}}
 ASK=true
 
 while getopts ":y" opt; do
   case $opt in
     y)
       DESTINATION=$2
+      PROFILE_NAME=${3:-${profile_name_def}}
+      PROFILE_CODE=${4:-${profile_code_def}}
       ASK=false
       ;;
     \?)
@@ -112,6 +124,33 @@ projects[drupal] = 7
 EOF
 fi
 
+# Prepare the install profile.
+if [ $PROFILE_NAME != "x" ] && [ -d profile_tmpl ] && [ -d profile_tmpl/ln_kickstart ]; then
+  echo "Preparing install profile ${PROFILE_CODE}_kickstart..."
+  if [ ! -d profiles ]; then
+    mkdir profiles
+  fi
+  rm -rf profile_tmp
+  cp -r profile_tmpl profile_tmp
+  cd profile_tmp/ln_kickstart
+
+  for file in $ln_*
+  do
+    mv "$file" "${PROFILE_CODE}_${file#ln_}"
+    sed -i "" "s/{PROFILE_NAME}/${PROFILE_NAME}/g" "${PROFILE_CODE}_${file#ln_}"
+    sed -i "" "s/{PROFILE_CODE}/${PROFILE_CODE}_kickstart/g" "${PROFILE_CODE}_${file#ln_}"
+  done
+
+  # now do search and replace in all files for {PROFILE_NAME} and {PROFILE_CODE}
+  cd ..
+  rm -rf ../profiles/${PROFILE_CODE}_kickstart
+  echo "Creating install profile ${PROFILE_CODE}_kickstart..."
+  rm -rf profiles/${PROFILE_CODE}_kickstart
+  mv ln_kickstart ../profiles/${PROFILE_CODE}_kickstart
+  cd ..
+  rm -rf profile_tmp
+fi
+
 # Build the distribution and copy the profile in place.
 echo "Building the distribution..."
 drush make drupal-org-core.make $TEMP_BUILD
@@ -120,6 +159,10 @@ cp -r tmp/* $TEMP_BUILD/sites/all
 rm -rf tmp
 if [ -d profiles ]; then
   cp -r profiles/* $TEMP_BUILD/profiles
+  rm -rf profiles/${PROFILE_CODE}_kickstart
+  if [ $profiles_folder -eq 0 ]; then
+    rm -rf profiles
+  fi
 fi
 mv $TEMP_BUILD $DESTINATION
 echo "done"
--- scripts/build_profile.sh
+++ scripts/build_profile.sh
@@ -7,24 +7,34 @@ set -e
 # Usage: scripts/build_profile.sh <profile_name> <profile_code> from the profile main directory.
 #
 
-PROFILE_NAME=$1
-PROFILE_CODE=$2
+profile_name_def="LN"
+profile_code_def="ln"
 
-rm -Rf profile_tmp
-cp -r profile_tmpl profile_tmp
-cd profile_tmp/ln_kickstart
+PROFILE_NAME=${1:-${profile_name_def}}
+PROFILE_CODE=${2:-${profile_code_def}}
 
-for file in $ln_*
-do
-  mv "$file" "${PROFILE_CODE}_${file#ln_}"
-  sed -i "" "s/{PROFILE_NAME}/${PROFILE_NAME}/g" "${PROFILE_CODE}_${file#ln_}"
-  sed -i "" "s/{PROFILE_CODE}/${PROFILE_CODE}_kickstart/g" "${PROFILE_CODE}_${file#ln_}"
-done
+if [ -d profile_tmpl ] && [ -d profile_tmpl/ln_kickstart ]; then
+  echo "Preparing install profile ${PROFILE_CODE}_kickstart..."
+  if [ ! -d profiles ]; then
+    mkdir profiles
+  fi
+  rm -rf profile_tmp
+  cp -r profile_tmpl profile_tmp
+  cd profile_tmp/ln_kickstart
 
-# now do search and replace in all files for {PROFILE_NAME} and {PROFILE_CODE}
+  for file in $ln_*
+  do
+    mv "$file" "${PROFILE_CODE}_${file#ln_}"
+    sed -i "" "s/{PROFILE_NAME}/${PROFILE_NAME}/g" "${PROFILE_CODE}_${file#ln_}"
+    sed -i "" "s/{PROFILE_CODE}/${PROFILE_CODE}_kickstart/g" "${PROFILE_CODE}_${file#ln_}"
+  done
 
-cd ..
-rm -Rf ../profiles/${PROFILE_CODE}_kickstart
-mv ln_kickstart ../profiles/${PROFILE_CODE}_kickstart
-cd ..
-rm -Rf profile_tmp
+  # now do search and replace in all files for {PROFILE_NAME} and {PROFILE_CODE}
+  cd ..
+  rm -rf ../profiles/${PROFILE_CODE}_kickstart
+  echo "Creating install profile ${PROFILE_CODE}_kickstart..."
+  rm -rf profiles/${PROFILE_CODE}_kickstart
+  mv ln_kickstart ../profiles/${PROFILE_CODE}_kickstart
+  cd ..
+  rm -rf profile_tmp
+fi
