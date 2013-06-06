30726f66a521da6e9a81fdd3150c403c4f81bf84
Nathan Lisgo <nathan@i-kos.com>
2013-06-06 12:50:03 +0100

    Insert dependency for controller module in profile info file

--- scripts/build_controller.sh
+++ scripts/build_controller.sh
@@ -4,7 +4,7 @@ set -e
 #
 # Prepare controller module
 #
-# Usage: scripts/build_controller.sh <profile_name> <profile_code> from the profile main directory.
+# Usage: scripts/build_controller.sh <profile_name> <profile_code> <profile_dir> <module_dir> from the profile main directory.
 #
 
 codeprepare () {
@@ -17,8 +17,15 @@ PROFILE_NAME=${1:-${profile_name_def}}
 
 profile_code_def=$( codeprepare $PROFILE_NAME )
 PROFILE_CODE=${2:-${profile_code_def}}
+PROFILE_DIR=${3:-profiles/${PROFILE_CODE}_kickstart}
+PROFILE_INFO=${PROFILE_DIR}/${PROFILE_CODE}_kickstart.info
+MODULE_DIR=${4:-tmp/modules/custom}
 
-if [ -d sites_all/modules/custom ] && [ ! -d sites_all/modules/custom/${PROFILE_CODE}_controller ] && [ -d controller_tmpl ] && [ -d controller_tmpl/ln_controller ]; then
+if [ -d $MODULE_DIR ]; then
+  mkdir -p $MODULE_DIR
+fi
+
+if [ ! -d sites_all/modules/custom/${PROFILE_CODE}_controller ] && [ -d controller_tmpl ] && [ -d controller_tmpl/ln_controller ]; then
   echo "Preparing controller module for ${PROFILE_CODE}_kickstart..."
   rm -rf controller_tmp
   cp -r controller_tmpl controller_tmp
@@ -39,4 +46,22 @@ if [ -d sites_all/modules/custom ] && [ ! -d sites_all/modules/custom/${PROFILE_
   echo "Creating controller module for ${PROFILE_CODE}_kickstart..."
   mv controller_tmp/ln_controller sites_all/modules/custom/${PROFILE_CODE}_controller
   rm -rf controller_tmp
+fi
+
+if [ -f $PROFILE_INFO ]; then
+  DEPENDENCY=`grep -n "^[ ]*dependencies[ ]*\[\][ ]*=[ ]*${PROFILE_CODE}_controller[ ]*$" $PROFILE_INFO | cut -f1 -d: | head -n 1`
+  if [ ! $DEPENDENCY ]; then
+    DEPENDENCY_MARKER=`grep -n "^[ ]*\;[ ]*Install Controller module[ ]*$" $PROFILE_INFO | cut -f1 -d: | head -n 1`
+    DEPENDENCY_TEXT="dependencies[] = ${PROFILE_CODE}_controller"
+    if [ $DEPENDENCY_MARKER ]; then
+      DEPENDENCY_LINE=$(($DEPENDENCY_MARKER+1))
+      sed -i "" ''$DEPENDENCY_LINE'i\'$'\n'''$DEPENDENCY_TEXT''$'\n''' "$PROFILE_INFO"
+    else
+      echo >> $PROFILE_INFO
+      echo >> $PROFILE_INFO
+      echo "; Install Controller module" >> $PROFILE_INFO
+      echo $DEPENDENCY_TEXT >> $PROFILE_INFO
+    fi
+    echo "Dependency for controller module inserted in ${PROFILE_INFO}"
+  fi
 fi
